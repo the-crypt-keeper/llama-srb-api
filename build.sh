31fa9f1f64335bc416ae7e10976a82ed973827e0
Mike <kryptk@gmail.com>
2025-01-17 02:01:44 +0000

    easier build process

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 git clone https://github.com/ggerganov/llama.cpp.git
 cd llama.cpp
 git checkout c05e8c9934f94fde49bc1bc9dc51eed282605150
-git apply ../llama-batch-api.patch
+cp -f ../batched.cpp ./examples/batched/
 rm -rf build
 cmake -B build -DGGML_CUDA=1 -DCMAKE_CUDA_ARCHITECTURES=61
 cmake --build build --config Release --target llama-batched -j
